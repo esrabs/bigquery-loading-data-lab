cfce32677cc2400048b6df8865f2f053baa859dc
Esrae Ben Selma <isra.ben.selma@gmail.com>
2025-12-02 16:12:19 +0100

    Add script to load CSV from GCS to BigQuery

---
+++ commands/bigquery_load_gcs.sh
@@ -0,0 +1,7 @@
+# Command to load a CSV from Google Cloud Storage into BigQuery
+bq load \
+  --source_format=CSV \
+  --autodetect \
+  --noreplace \
+  nyctaxi.2018trips \
+  gs://cloud-training/OCBL013/nyc_tlc_yellow_trips_2018_subset_2.csv
